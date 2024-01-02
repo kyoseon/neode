4f2a8877efb85979affe81ea9b96555a110a5dad
kyoseon <kyoseonk@gmail.com>
2024-01-03 01:19:14 +0900

    keymaps for buffer close instead of quit.

--- lua/vim-keymaps.lua
+++ lua/vim-keymaps.lua
@@ -1,11 +1,13 @@
 vim.g.mapleader = ","
 
-vim.keymap.set('n', '<leader>q', ':q<CR>', {})
-vim.keymap.set('n', '<leader>Q', ':q!<CR>', {})
-vim.keymap.set('n', '<leader>s', ':w<CR>', {})
-vim.keymap.set('n', '<leader>n', ':e#<CR>', {})
+vim.keymap.set("n", "<leader>1", ":bnext<CR>", {})
+vim.keymap.set("n", "<leader>2", ":bprevious<CR>", {})
+vim.keymap.set("n", "<leader>q", ":bdelete<CR>", {})
+vim.keymap.set("n", "<leader>Q", ":bdelete!<CR>", {})
+vim.keymap.set("n", "<leader>s", ":w<CR>", {})
 
-vim.keymap.set('n', '<C-r>', ':noh<CR>', {})
-vim.keymap.set('n', '<leader>c', ':set cursorcolumn!<CR>', {})
-vim.keymap.set('n', '<TAB>', '<C-w>l', {})
-vim.keymap.set('n', '<leader><TAB>', '<C-w>h', {})
+vim.keymap.set("n", "<C-r>", ":noh<CR>", {})
+vim.keymap.set("n", "<leader>c", ":set cursorcolumn!<CR>", {})
+
+vim.keymap.set("n", "<TAB>", "<C-w>l", {})
+vim.keymap.set("n", "<leader><TAB>", "<C-w>h", {})
