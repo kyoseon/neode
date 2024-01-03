9374022d7cbf8c9db677d1bdf2def245bd634c35
kyoseon <kyoseonk@gmail.com>
2024-01-04 03:30:34 +0900

    keymaps for buffer

--- lua/vim-keymaps.lua
+++ lua/vim-keymaps.lua
@@ -1,10 +1,10 @@
 vim.g.mapleader = ","
 
-vim.keymap.set("n", "<leader>1", ":bnext<CR>", {})
-vim.keymap.set("n", "<leader>2", ":bprevious<CR>", {})
-vim.keymap.set("n", "<leader>q", ":bdelete<CR>", {})
-vim.keymap.set("n", "<leader><C-q>", ":bdelete!<CR>", {})
-vim.keymap.set("n", "<leader>Q", ":qa<CR>", {})
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>", {})
+vim.keymap.set("n", "<leader>bb", ":bprevious<CR>", {})
+vim.keymap.set("n", "<leader>bd", ":bdelete<CR>", {})
+vim.keymap.set("n", "<leader>bf", ":bdelete!<CR>", {})
+vim.keymap.set("n", "<leader>z", ":qa<CR>", {})
 vim.keymap.set("n", "<leader>s", ":w<CR>", {})
 
 vim.keymap.set("n", "<leader>r", ":noh<CR>", {})
