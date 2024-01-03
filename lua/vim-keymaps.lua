d64cffad97e458c2f0d6c4559a381b2a9ffa6789
kyoseon <kyoseonk@gmail.com>
2024-01-03 17:56:14 +0900

    :noh keymap confused with redo keymap

--- lua/vim-keymaps.lua
+++ lua/vim-keymaps.lua
@@ -6,8 +6,5 @@ vim.keymap.set("n", "<leader>q", ":bdelete<CR>", {})
 vim.keymap.set("n", "<leader>Q", ":bdelete!<CR>", {})
 vim.keymap.set("n", "<leader>s", ":w<CR>", {})
 
-vim.keymap.set("n", "<C-r>", ":noh<CR>", {})
+vim.keymap.set("n", "<leader>r", ":noh<CR>", {})
 vim.keymap.set("n", "<leader>c", ":set cursorcolumn!<CR>", {})
-
-vim.keymap.set("n", "<TAB>", "<C-w>l", {})
-vim.keymap.set("n", "<leader><TAB>", "<C-w>h", {})
