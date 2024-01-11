cb4822ac66ec24de68a33480dc5065a3dfce764d
kyoseon <kyoseonk@gmail.com>
2024-01-11 22:22:53 +0900

    moves color settings in gruvbox plugin.

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -3,4 +3,18 @@ return {
 	lazy = false,
 	name = "gruvbox",
 	priority = 1000,
+
+	config = function()
+		local gruvbox = require("gruvbox")
+
+		gruvbox.setup({
+			terminal_colors = true,
+			transparent_mode = true,
+			overrides = {
+				SignColumn = { bg = transparent },
+				DapBreakpoint = { fg = "#78bcdd" },
+			},
+		})
+		vim.cmd("colorscheme gruvbox")
+	end,
 }
